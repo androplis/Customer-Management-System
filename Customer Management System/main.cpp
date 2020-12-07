1b059d8cbe3d15f8dfff6363284c9a4ae14dd8b0
Andrew Biddle <andrewbiddlebugs@gmail.com>
2020-12-07 17:44:56 -0500

    Initial Commit

---
+++ Customer Management System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Customer Management System
+//
+//  Created by Andrew Biddle on 12/7/20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
